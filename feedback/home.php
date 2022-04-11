3158cb2244a7e255bbd1e04d608408d58469c7c8
Han Kwan <saihankwansaing@gmail.com>
2022-04-11 14:05:45 +0630

    making connection

--- feedback/home.php
+++ feedback/home.php
@@ -1,4 +1,5 @@
 <?php
-    $pdo = new PDO('mysql:host=localhost;port=3308;dbname=products_crud', 'root', '');
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $pdo = new PDO('mysql:host=localhost;port=3308;dbname=products_crud', 'root', '');
+    // $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo 'hello';
 ?>
