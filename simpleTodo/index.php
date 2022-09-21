f91ef2c3a20aca2c9a18609e10bac5cccb780a51
Han Kwan <saihankwansaing@gmail.com>
2022-09-21 20:06:53 +0630

    just add style :)

--- simpleTodo/index.php
+++ simpleTodo/index.php
@@ -10,29 +10,38 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Simple todo</title>
 </head>
-<body>
-    <form action="addTodo.php" method="post">
-        <input type="text" name="todoName">
-        <button>Add todo</button>
-    </form>
-    <div style="margin-top: 20px;">
-        <?php foreach ($todos as $todoName => $todo): ?>
-            <div style="margin-bottom: 10px;">
-                <form action="check.php" method="post" style="display: inline-block;">
-                    <input type="hidden" name="checking" value="<?php echo $todoName ?>">
-                    <input type="checkbox" class="checkTodo" <?php echo $todo['completed'] ? 'checked' : '' ?>>
-                </form>
-        
-                <?php echo $todoName ?>
+<body class="container">
+    <div class="mt-5 row justify-content-center">
+        <div class="col-5">
+            <form action="addTodo.php" method="post" class="align-items-center d-flex">
+                <input type="text" name="todoName">
+                <button class="ms-4 btn btn-primary btn-sm">Add todo</button>
+            </form>
+            <div class="mt-3">
+                <?php foreach ($todos as $todoName => $todo): ?>
+                    <div style="margin-bottom: 10px;">
+                        <form action="check.php" method="post" style="display: inline-block;">
+                            <input type="hidden" name="checking" value="<?php echo $todoName ?>">
+                            <input type="checkbox" class="checkTodo" <?php echo $todo['completed'] ? 'checked' : '' ?>>
+                        </form>
+                
+                        <div style="display: inline-block; margin-left: 5px;" class="w-75">
+                            <div class="d-flex align-items-center justify-content-between">
+                                <?php echo $todoName ?>
 
-                <form style="display: inline-block; margin-left: 30px;" action="delete.php" method="post">
-                    <input type="hidden" name="deleteTodo" value="<?php echo $todoName ?>">    <!-- don't forget to echo -->
-                    <button>Delete</button>
-                </form>
+                                <form action="delete.php" method="post">
+                                    <input type="hidden" name="deleteTodo" value="<?php echo $todoName ?>">    <!-- don't forget to echo -->
+                                    <button class="btn btn-danger btn-sm">Delete</button>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
             </div>
-        <?php endforeach; ?>
+        </div>
     </div>
 
 <script>
